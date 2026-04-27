d0124a176ec05bfe90214c2b39bc93b980954f3d
Elisei <eli10es@yahoo.es>
2026-04-27 21:43:03 +0300

    Created Location interface, added random creation of cars, added 4 cars on app-component

--- autonomous-cars/src/app/Components/car/car.component.ts
+++ autonomous-cars/src/app/Components/car/car.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Car } from '../../Interfaces/car';
+import { Location } from '../../Interfaces/location';
 
 @Component({
   selector: 'app-car',
   imports: [],
   templateUrl: './car.component.html',
-  styleUrl: './car.component.css'
+  styleUrl: './car.component.css',
 })
-export class CarComponent {
+export class CarComponent implements OnInit, Car, Location {
+  manufacturer!: string;
+  model!: string;
+  ID!: number;
+  x!: number;
+  y!: number;
+  speed!: number;
 
+  constructor() {}
+
+  randomCar(): {
+    manufacturer: string;
+    model: string;
+    ID: number;
+    x: number;
+    y: number;
+    speed: number;
+  } {
+    const models = ['A4', 'A6', 'A8', 'R8', 'RS7'];
+
+    const randomModel = models[Math.floor(Math.random() * models.length)];
+    const randomID = Math.floor(Math.random() * 10) + 1;
+    const randomX = Math.floor(Math.random() * 100);
+    const randomY = Math.floor(Math.random() * 100);
+    const randomSpeed = Math.floor(Math.random() * 120) + 1;
+    return {
+      manufacturer: 'Audi',
+      model: randomModel,
+      ID: randomID,
+      x: randomX,
+      y: randomY,
+      speed: randomSpeed,
+    };
+  }
+
+  ngOnInit(): void {
+    const car = this.randomCar();
+    this.manufacturer = car.manufacturer;
+    this.model = car.model;
+    this.ID = car.ID;
+    this.x = car.x;
+    this.y = car.y;
+    this.speed = car.speed;
+    console.log(car);
+  }
 }
--- autonomous-cars/src/app/app.component.ts
+++ autonomous-cars/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarComponent } from './Components/car/car.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'autonomous-cars';
